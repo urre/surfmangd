ad7eb508e227b0eb5d2eef5e149595f4ac1a56f1
Urban Sanden <hej@urre.me>
2017-04-03 09:48:05 +0200

    Update affiliate links

--- src/components/Affiliates.js
+++ src/components/Affiliates.js
@@ -15,10 +15,10 @@ class Affiliates extends Component {
 			</Box>
 
 			<Affiliate name='Vimla' imageurl='https://res.cloudinary.com/urre/image/upload/v1490959449/vimla_f7hrlu.png' description='Mobilabonnemang med fastpris utan bindningstid' url='https://track.adtraction.com/t/t?a=1081333617&as=1177346381&t=2&tk=1' />
-			<Affiliate name='Fello' imageurl='https://res.cloudinary.com/urre/image/upload/v1490959448/fello_kaztsd.png' description='Mobilabonnemang med sunt förnuft och schyssta villkor' url='https://www.fello.se/' />
-			<Affiliate name='Halebop' imageurl='https://res.cloudinary.com/urre/image/upload/v1490959448/halebop_q3m39t.png' description='Den schyssta mobiloperatören' url='https://shop.halebop.se/mobilabonnemang' />
-			<Affiliate name='Hallon' imageurl='https://res.cloudinary.com/urre/image/upload/v1490959449/hallon_asrw1k.png' description='Mobilabonnemang från din nya mobiloperatör' url='https://www.hallon.se/' />
-			<Affiliate name='Telenor' imageurl='https://res.cloudinary.com/urre/image/upload/v1490959448/telenor_xzf83c.png' description='Den flexibla mobiloperatören' url='http://www.telenor.se/abonnemang/' />
+			<Affiliate name='Fello' imageurl='https://res.cloudinary.com/urre/image/upload/v1490959448/fello_kaztsd.png' description='Mobilabonnemang med sunt förnuft och schyssta villkor' url='https://track.adtraction.com/t/t?a=1074737227&as=1177346381&t=2&tk=1' />
+			<Affiliate name='Halebop' imageurl='https://res.cloudinary.com/urre/image/upload/v1490959448/halebop_q3m39t.png' description='Den schyssta mobiloperatören' url='https://track.adtraction.com/t/t?a=1141832310&as=1177346381&t=2&tk=1' />
+			<Affiliate name='Hallon' imageurl='https://res.cloudinary.com/urre/image/upload/v1490959449/hallon_asrw1k.png' description='Mobilabonnemang från din nya mobiloperatör' url='https://track.adtraction.com/t/t?a=1083250335&as=1177346381&t=2&tk=1' />
+			<Affiliate name='Telenor' imageurl='https://res.cloudinary.com/urre/image/upload/v1490959448/telenor_xzf83c.png' description='Den flexibla mobiloperatören' url='https://track.adtraction.com/t/t?a=1086470696&as=1177346381&t=2&tk=1' />
 		</Flex>
     	
     )
