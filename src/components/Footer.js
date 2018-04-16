61885d30f91c9b6fcbd6efd6fec8465232fc3611
Urban Sanden <hej@urre.me>
2018-04-16 21:26:50 +0200

    Add github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Box } from 'reflexbox'
 	const Footer = () => (
 	<Box className='footer center' col={ 12 } p={3}>
 		<p><small>* Siffrorna är inte exakta eftersom de påverkas av de tjänster du använder/sidor du besöker och på hur stor datamängd just den musik och den video du streamar kräver. Det bygger också på just nu på att du förbrukar data i alla de tre kategorierna.</small></p>
-		<p><small>Ett projekt från <a href="https://sprintworks.se">Sprint Works</a></small></p>
+		<p><small>Ett projekt från <a href="https://sprintworks.se">Sprint Works</a>. Kod på <a href="https://github.com/urre/surfmangd">Github</a></small></p>
 	</Box>
 	)
 
